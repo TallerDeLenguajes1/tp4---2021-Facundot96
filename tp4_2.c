f7607569ec66ac6d0455d1e4e4934a0483d88153
Facundot96 <facundotapia.96@gmail.com>
2021-05-06 00:00:53 -0300

    Create tp4_2.c
    Se agrega el primer archivo .c

---
+++ tp4_2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+
+typedef struct Tarea {
+	int TareaID; //Numerado en ciclo iterativo
+	char* Descripcion; //
+	int Duracion; // entre 10 – 100
+}Tarea;
+
+
+
+
+int main() {
+
+
+	printf("Hello World\n");
+	return 0;
+
+
+}
